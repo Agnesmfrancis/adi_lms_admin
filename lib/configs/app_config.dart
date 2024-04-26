7596582efd18108590a3b40429865388203647ed
Agoo <agnesmarinf@gmail.com>
2024-04-26 13:01:26 +0530

    first commit

---
+++ lib/configs/app_config.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppConfig {
+  // App Name
+  static const String appName = 'Adi LMS';
+
+  // Default theme color - Change Hex code <3F51B5>
+  static const Color themeColor = Color(0xFF3F51B5);
+
+  static const Color titleBarColor = Color.fromARGB(255, 233, 234, 238);
+  static const scffoldBgColor = Color(0xFFf0f0f3);
+
+  // Firebase server key (Push Notifictaion)
+  static const String firebaseServerKey = 'BDYRtXNZb-xHikLdSRvNtlf7hHScsPVYUqNU90AXRQpsx_EVmvkjMIzLL_MGHhct9h91Tvmsqwzhgo3HvNSK4cU';
+}
---
+++ lib/configs/assets_config.dart
@@ -0,0 +1,13 @@
+class AssetsConfig {
+  // LOGO
+  static const String logo = 'assets/images/logo.png';
+  static const String logoDark = 'assets/images/logo_dark.png';
+  static const String adilogo = 'assets/images/GROUP_OF_INSTITUTE_NEW_LOGO.png';
+
+
+  // OTHERS (SVG)
+  static const String loginImageString = 'assets/images/login.svg';
+  static const String verifyImageString = 'assets/images/verify.svg';
+
+  static const String noDataImage = 'assets/images/no_data.png';
+}
